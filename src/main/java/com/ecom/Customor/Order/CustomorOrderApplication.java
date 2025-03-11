230ab6870397fae47b83981f5f038cddafaa14c7
karthik06ce <mspvl12052006ce@gmail.com>
2025-03-11 10:16:24 +0530

    Initial commit

---
+++ src/main/java/com/ecom/Customor/Order/CustomorOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.Customor.Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomorOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomorOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/Customor/Order/CustomorOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Customor.Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomorOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
